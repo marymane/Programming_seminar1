99bd5268f76bfdc1c213c37758998f29a834207d
Maria Grivastova <marymane@yandex.ru>
2022-10-06 16:40:22 +0700

    All even numbers

---
+++ Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int remain = 0;
+Console.WriteLine("Все четные числа до " + N + ":");
+while (i<=N)
+{
+    remain = i % 2;
+         if (remain == 0)
+        {	  
+	     Console.WriteLine(i);
+		 i++;
+	    }
+        else
+		{
+		i++;
+		}	
+}
