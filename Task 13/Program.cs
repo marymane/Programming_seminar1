17d0e976a2611749adfda13268053144ec6f6836
Maria Grivastova <marymane@yandex.ru>
2022-10-09 00:31:36 +0700

    Third digit counting left to right

---
+++ Task 13/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+
+int CreateNumber()
+{
+return  new Random().Next(10,1000);
+}
+
+int ThirdNumber (int Number)
+{
+return (Number % 10);
+}
+ 
+ int num = CreateNumber();
+ Console.WriteLine("Имеем число " + num);
+ if (num < 100)
+    {
+    Console.WriteLine("Число двузначное, третьей цифры нет");
+    }
+else 
+    {
+    Console.WriteLine("Третья цифра - " + ThirdNumber (num));  
+    }
+ 
