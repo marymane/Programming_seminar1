7a31b7d38b2d4f82765cc075267ef50ad997767f
Maria Grivastova <marymane@yandex.ru>
2022-10-09 00:02:15 +0700

    Second digit of Number

---
+++ Task10/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+int CreateNumber()
+{
+return  new Random().Next(100,1000);
+}
+
+int SecondDigit (int Number)
+{
+
+return ((Number % 100)/10);
+}
+ int num = CreateNumber();
+ Console.WriteLine("Трехзначное число: " + num);
+ Console.WriteLine("Второй разряд числа равен " + SecondDigit (num));
