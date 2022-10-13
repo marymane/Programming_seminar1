24e071284fe7f1ca8e9a33eef8da43c0cf699de3
Maria Grivastova <marymane@yandex.ru>
2022-10-13 17:05:05 +0700

    table of third degree

---
+++ Task23/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+ 
+int GetNumber()
+{
+    
+    Console.WriteLine("Введите число!");
+    int number = Convert.ToInt32(Console.ReadLine());  
+    return number;
+}
+
+void TableOfThirdDegree (int number)
+	{
+	int i=1;
+	while (i<=number)
+		{
+		Console.WriteLine($"{i}*{i}*{i} = {Math.Pow(i,3)}");
+        i++;
+		}
+	}
+	
+int N = GetNumber();
+TableOfThirdDegree(N);
