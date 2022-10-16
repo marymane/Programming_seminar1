bde9584319fdb9a07787e7a2194540700637ff1c
Maria Grivastova <marymane@yandex.ru>
2022-10-16 15:05:28 +0700

    Sum of digits

---
+++ Task27/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetNumber()
+{
+    Console.WriteLine("Введите многозначное число");
+    int number = Convert.ToInt32(Console.ReadLine());  
+    return number;
+}
+
+int GetSumOfDigits (int num)
+
+{
+	int sum = 0;
+	while (num>0)
+	{
+	sum+=num%10;
+	num/=10;
+	}
+return sum;
+}
+
+int N = GetNumber();
+
+Console.WriteLine($"Сумма всех разрядов числа равна {GetSumOfDigits (N)}");
