cd15b2ff4a35d2269dba046636dc4429f563c1f5
Maria Grivastova <marymane@yandex.ru>
2022-10-20 16:51:56 +0700

    Number of Even Array Elements

---
+++ Task34/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+ 
+
+
+int[] array = new int[10];
+
+void FillArray (int[] array)
+{
+    int length = array.Length; 
+    for(int i = 0; i<length; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+    }
+    
+}
+
+void PrintArray (int[] arr)
+{
+    int l = arr.Length; 
+    for(int i = 0; i<l; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    
+}
+
+int NumberOfEven (int[] arr)
+{
+    int l = arr.Length;
+    int count = 0;
+    for(int i=0;i<l;i++)
+    {
+        if (arr[i]%2==0)
+        {
+            count+=1;
+        }
+    }
+    return count;
+}
+
+FillArray (array);
+PrintArray (array);
+Console.WriteLine ("->" + NumberOfEven(array) + " четных элемента");
