5f4a16c7ede79db4b99c87bc72132afa9ac35eb8
Maria Grivastova <marymane@yandex.ru>
2022-10-26 17:11:25 +0700

    Rcursion for Printing Natural Numbers from N to 1

---
+++ Task64/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void PrintAllNatural (int N)
+{
+    if (N == 1) 
+    Console.Write (N);
+
+    else
+    {
+        Console.Write (N + ",");
+        N--;
+        PrintAllNatural (N);
+    }
+}
+
+int N = Convert.ToInt32(Console.ReadLine());
+PrintAllNatural (N);
