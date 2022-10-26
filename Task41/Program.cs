2374cb4b95e88545d82e6972822cacd76b8a62c5
Maria Grivastova <marymane@yandex.ru>
2022-10-26 17:08:58 +0700

    Filling matrix with Double Numbers

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,34 +1,29 @@
-﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
 //Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-void FillArray (int[] array)
+void FillArray(int[] array)
 {
-    
-    for(int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"Введите {i+1} число: ");
+        Console.Write($"Введите {i + 1} число: ");
         array[i] = Convert.ToInt32(Console.ReadLine());
     }
-    
 }
 
-void PrintArray (int[] arr)
+void PrintArray(int[] arr)
 {
-    
-    for(int i = 0; i < arr.Length; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
         Console.Write($"{arr[i]} ");
     }
-    
 }
-
 
 int PositiveElements(int[] arr)
 {
     int counter = 0;
-    for(int i = 0; i < arr.Length; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
         if (arr[i] > 0)
         {
@@ -38,8 +33,6 @@ int PositiveElements(int[] arr)
 
     return counter;
 }
-
-
 
 Console.WriteLine("Сколько чисел будем вводить?");
 int M = Convert.ToInt32(Console.ReadLine());
---
+++ Task47/Program.cs
@@ -0,0 +1,50 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, 
+//заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+
+ 
+
+// //int m = Convert.ToInt32(Console.ReadLine());
+// //int m = GetDimention ();
+// //int n = GetDimention ();
+// double[,] array = new double [5,8];
+// FillArray(array);
+// PrintArray(array);
+
+void FillArray (double[,] array)
+{
+Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.NextDouble() * 10 - 20; // NextDouble() дает случайное вещественное число в диапазоне от 0 до 1
+        }
+        
+    }
+}
+
+void PrintArray (double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("{0,6:F2} ", array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+  
+  
+Console.Write ("Введите размерность массива m ");
+int m = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите размерность массива n ");
+int n = Convert.ToInt32 (Console.ReadLine());
+
+double[,] matrix = new double[m,n];
+FillArray (matrix);
+PrintArray (matrix);
