2db91a01beac378646924bf8d3b638ffcd6bbca3
Maria Grivastova <marymane@yandex.ru>
2022-10-27 13:17:27 +0700

    Find Average in Every Column

---
+++ Task52/Program.cs
@@ -0,0 +1,54 @@
+﻿// /Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Cреднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+void FillArray (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next (0,10);
+        }
+    }
+
+}
+
+void PrintArray (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]}   ");
+        }
+    Console.WriteLine();
+    }
+    
+}
+
+void FindAverage (int[,] array)
+{
+    double average = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            average += array[i,j];
+        }
+
+        Console.Write($"{Math.Round(average/array.GetLength(0), 1)} ");
+        average = 0;
+    }
+
+}
+
+
+int[,] matrix = new int [5,5];
+FillArray (matrix);
+PrintArray (matrix);
+FindAverage (matrix);
