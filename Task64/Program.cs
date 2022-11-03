e697e8d292de370c0496e8f5b9f8ef9da5881206
Maria Grivastova <marymane@yandex.ru>
2022-11-03 17:10:30 +0700

    Ackermann Function

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -17,5 +17,48 @@ void PrintAllNatural (int N)
     }
 }
 
+
+double GetNumberFromConsole()
+{
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+void WriteNumbers(double lastNumber)
+{
+    int counter = 1;
+    while(counter <= lastNumber)
+    {
+        if (counter + 1 <= lastNumber)
+            Console.Write(counter + ", ");
+        else
+            Console.Write(counter);
+        counter++;
+    }
+}
+
+string WriteNumbersRecursion(int startNumber, double lastNumber)
+{
+    if (startNumber <= lastNumber)
+        return startNumber + ", " + WriteNumbersRecursion(startNumber+1, lastNumber);
+    
+    return string.Empty;
+}
+
+void WriteNumbersRecursionVoid(int startNumber, double lastNumber)
+{
+    if (startNumber <= lastNumber)
+    {
+        Console.Write(startNumber + ", ");
+        WriteNumbersRecursionVoid(startNumber+1, lastNumber);
+    }     
+}
+
+double number = GetNumberFromConsole();
+WriteNumbers(number);
+Console.WriteLine();
+Console.Write(WriteNumbersRecursion(1, number));
+Console.WriteLine();
+WriteNumbersRecursionVoid(1, number);
+
 int N = Convert.ToInt32(Console.ReadLine());
 PrintAllNatural (N);
---
+++ Task68/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+int AckermannFunction (int numberM, int numberN)
+{ 
+    if (numberM == 0) 
+    return numberN + 1; 
+    if (numberM != 0 && numberN == 0) 
+    return AckermannFunction(numberM - 1, 1); 
+    if (numberM > 0 && numberN > 0) 
+    return AckermannFunction(numberM - 1, AckermannFunction(numberM, numberN - 1));
+    return AckermannFunction(numberM, numberN);
+}
+
+Console.WriteLine("Вычислим функцию Аккермана для двух неотрицательных чисел m и n");
+Console.WriteLine("Введите число m:");
+int numberM = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите число n:");
+int numberN = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine($"Функция Аккермана для чисел A({numberM},{numberN}) = {AckermannFunction(numberM, numberN)}");
